07f02ecdf37842f63a18a0ba1711e21f9276bae2
RockTharsh <52143660+RockTharsh@users.noreply.github.com>
2021-08-29 10:24:36 -0400

    Started working with directory getter.

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+
+working = True #This bool indicates that the program is working.
+def askfordirectory(directory):
+
+
+
+
+
+
+while working:
+    print('Welcome to file ramdomizer. First of all select your path.')
+    input()
