9b87739a9e2d4d057cdce54c75cf6ed7588b7cc6
RockTharsh <52143660+RockTharsh@users.noreply.github.com>
2021-08-30 15:08:08 -0400

    Added operative system recognition.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import colorama as color
+import platform
 working = True
+actualOs = platform.system()
 
 
 # This method ask if the     path exist or not.
 def ask_for_directory():
-
     path = str(input('First of all select your path. Write down below your path:'))
     if not os.path.isdir(path):
         print(color.Fore.RED + 'Error 00. You have added an wrong syntax path. Please try again.', os.path.isfile(path))
@@ -15,5 +16,5 @@ def ask_for_directory():
 
 
 while working:
-    print('Welcome to file randomizer.')
+    print('Welcome to file randomizer.\nYou are running this script on a', actualOs, 'operative system')
     ask_for_directory()
